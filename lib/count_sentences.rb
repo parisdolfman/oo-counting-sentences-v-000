0142ef3617ef83e17f89e1b107982d959f127959
Paris Dolfman <parisdolfman@gmail.com>
2018-08-27 19:21:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence = self.split(/[.?!]/)
+    sentence.delete_if{|sentence| sentence.empty?}
+    sentence.length
   end
+
 end
